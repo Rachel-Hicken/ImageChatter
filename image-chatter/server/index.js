a51f603543c51cbe0d9b5c4dbe713f403d311500
Rachel Hicken <rachkh22@gmail.com>
2018-04-27 13:51:59 -0600

    setup

---
+++ image-chatter/server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+
+
+
+
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`The server is listening to ${port}`)
+})
