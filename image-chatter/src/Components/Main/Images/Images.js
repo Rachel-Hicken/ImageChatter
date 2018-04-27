e270ec0f1af1a912d8540369bb17354d4fc2f461
Rachel Hicken <rachkh22@gmail.com>
2018-04-27 17:31:50 -0600

    fixed images error

--- image-chatter/src/Components/Main/Images/Images.js
+++ image-chatter/src/Components/Main/Images/Images.js
@@ -4,11 +4,12 @@ import './Images.css'
 export default function Images(){
     return(
         <div>
-        <img src=".\images\threatenedSwan.jpg" alt="Threatened Swan"/>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Jan_Asselijn_-_De_bedreigde_zwaan%3B_later_opgevat_als_allegorie_op_Johan_de_Witt_-_Google_Art_Project.jpg/450px-Jan_Asselijn_-_De_bedreigde_zwaan%3B_later_opgevat_als_allegorie_op_Johan_de_Witt_-_Google_Art_Project.jpg" 
+        alt="Threatened Swan"/>
 
-        {/* <img src=".\images\picasso.jpg" alt="warm/cool portrait"/>
-        <img src=".\images\Water-Lilies-Nympheas-Monet.jpg" alt="Water Lilies"/>
-        <img src=".\images\vanGogh.jpg" alt="Irises"/> */}
+        <img src="https://i.pinimg.com/564x/af/50/30/af50302c553657de6726a901575a3ce6.jpg" alt="warm/cool portrait"/>
+        <img src="https://www.claude-monet.com/images/paintings/water-lilies-claude-monet.jpg" alt="Water Lilies"/>
+        <img src="http://media.giuntiscuola.it/_tdz/@media_manager/2616690/" alt="Irises"/>
         </div>
 
     )
