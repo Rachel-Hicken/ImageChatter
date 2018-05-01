9e6daaec0a2211293dea20c76c8e7adc6e813a57
Rachel Hicken <rachkh22@gmail.com>
2018-04-30 21:48:13 -0600

    fixed star wars extras, need to add css

--- image-chatter/src/Components/Main/StarWars/StarWars.js
+++ image-chatter/src/Components/Main/StarWars/StarWars.js
@@ -6,23 +6,31 @@ export default class StarWars extends Component{
     constructor(){
         super()
         this.state={
-            tatooine: ''
+            tatooine: '',
+            falcon: ''
         }
     }
 
     componentDidMount(){
-        axios.get('https://swapi.co/api/planets/1').then((res)=>{
+        axios.get('https://swapi.co/api/planets/1/').then((res)=>{
             this.setState({
                 tatooine: res.data
             })
+        })
+        axios.get('https://swapi.co/api/starships/10/').then((res)=>{
+            falcon: res.data
         })
     }
 
     render(){
         return(
             <div>
-                <h1>Name: {this.state.tatooine.name}</h1> 
-                <h1>Climate: {this.state.tatooine.climate}</h1>
+                <h1>A Notable Planet</h1>
+                <h2>Name: {this.state.tatooine.name}</h2> 
+                <h2>Climate: {this.state.tatooine.climate}</h2>
+                <h1>The Coolest Starship</h1>
+                <h2>Name: {this.state.falcon.name}</h2>
+                <h2>Maximum Atmosphering Speed: {this.state.falcon.max_atmosphering_speed}</h2>
             </div>
         )
     }
